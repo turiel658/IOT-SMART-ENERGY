2ba5afa86e1cd46bae1322950d204400f4e1d69e
dylanMauro <d.cornejomiranda@uandresbello.edu>
2023-09-24 20:53:20 -0300

    primer commit 2

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Dimensions} from 'react-native';
+import {LineChart} from "react-native-chart-kit";
+import io from "socket.io-client"
+import React, {useState} from "react"
+
+const socket = io("http://localhost:3000")
 
 export default function App() {
+  const [data, setData] = useState([])
+  let listaParaActualizar = []
+
+  const recibirData = () => {
+    socket.on("dato", (dato) => {
+      console.log(dato)
+      if (listaParaActualizar.length === 5) {
+        setData(listaParaActualizar)
+        listaParaActualizar = []
+      } else {
+        listaParaActualizar.unshift(dato)
+      }
+
+    })
+  }
+  recibirData()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Inicio de sesión</Text>
+      <TextInput placeholder='Número de cliente' style={styles.inputText}/>
+      <TextInput placeholder='Contraseña'/>
+      <Button onPress={() => alert("presionado")} title="Presioname"/>
       <StatusBar style="auto" />
+      <LineChart data={{
+        labels: data,
+        datasets: [
+            {
+              data: data
+            }   
+        ] 
+      }}
+      width={Dimensions.get("window").width - 20}
+      height={200}
+      chartConfig={{
+        backgroundGradientFrom: "#0000FF",
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        strokeWidth: 2, // optional, default 3
+        barPercentage: 0.5,
+        useShadowColorFromDataset: false // optional
+      }}
+      style= {{
+        borderRadius: 10,
+        paddingTop: 10
+      }}
+      yAxisSuffix="W"
+      />
+      
     </View>
   );
 }
@@ -13,8 +62,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputText: {
+    backgroundColor: "#666666"
+  }
 });
